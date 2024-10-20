8bc71f7e338784c3dc89ad4edc70a994cf616d4a
Yevhenii-Tsariuk <care64678@gmail.com>
2024-10-20 14:25:07 +0300

    all done

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,16 @@
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+     const contacts = await readContacts();
+     
+    const newContacts = Array(number).fill(0).map(createFakeContact);
+
+    const result = [...contacts, ...newContacts];
+    writeContacts(result);
+    
+     
+};
+
+generateContacts(5);
---
+++ src/scripts/getAllContacts.js
@@ -0,0 +1,8 @@
+import { readContacts } from "../utils/readContacts.js";
+
+
+export const getAllContacts = async () => 
+     await readContacts();
+
+
+console.log(await getAllContacts());
